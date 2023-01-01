6813d638a46b886022fe568d5cbc15cd634b06fd
Saran-pariyar <spariyar05@gmail.com>
2023-01-01 19:47:59 +0545

    Made small changes in word"

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const wrapper_react = wrapper.getBoundingClientRect();
 const no_btn_react = no_btn.getBoundingClientRect();
 
 yes_btn.addEventListener("click",()=>{
-    ques.innerHTML = "I like you too!! :) " ;
+    ques.innerHTML = "I knew it!! " ;
 });
 
 no_btn.addEventListener("click",()=>{
